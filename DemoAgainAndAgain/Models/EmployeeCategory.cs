a053f87973b8e408044703390d73c28acc870d1a
Sveta <svetochkavlm@mail.ru>
2023-05-15 01:00:18 +0300

    Added dbcontext and styles

---
+++ DemoAgainAndAgain/Models/EmployeeCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoAgainAndAgain;
+
+public partial class EmployeeCategory
+{
+    public int EmployeeCategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
---
+++ DemoAgainAndAgain/Models/Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoAgainAndAgain;
+
+public partial class Service
+{
+    public int ServiceId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Photo { get; set; }
+
+    public int? Duration { get; set; }
+
+    public double? Cost { get; set; }
+
+    public int? Discount { get; set; }
+
+    public double? CostWithDiscount { get; set; }
+
+    public string? Discription { get; set; }
+
+    public virtual ICollection<ClientService> ClientServices { get; set; } = new List<ClientService>();
+
+    public virtual ICollection<ServiceAdditionalPhoto> ServiceAdditionalPhotos { get; set; } = new List<ServiceAdditionalPhoto>();
+}
